aed5233ed33ad355f94be14af2a7ac8dccd073a9
Jéssica Santiago <jessicabsanti@gmail.com>
2026-07-30 16:06:04 -0300

    Corrige SESSION_CONFIG_DEFAULTS

--- settings.py
+++ settings.py
@@ -17,6 +17,11 @@ SESSION_CONFIGS = [
     ),
 ]
 
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00,
+    participation_fee=0.00,
+    doc="",
+)
 
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
